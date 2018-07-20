4bd8f88600cb1bbf2b35a0a18519a9a30f9bdefb
ovrlod <farhan.khan@ovrlod.com>
2018-07-20 10:23:12 +0500

    first version commit

---
+++ dist/create.element-1.1.js
@@ -0,0 +1,64 @@
+/*!
+ * Create Element Plugin v1.0.0
+ * https://github.com/faudzif
+ * Copyright (c) 2018 Faudzif
+ */
+
+(function () {
+    this.createElement = function () {
+        if (arguments[0] && typeof arguments[0] === 'object') {
+            elementOptions(arguments[0])
+        } else {
+            console.log('Please create element\'s property');
+        }
+    };
+
+    function elementOptions(options) {
+        if (options.hasOwnProperty('element')) {
+            var element = document.createElement(options.element);
+            if (options.hasOwnProperty('id')) {
+                element.setAttribute('id', options.id);
+            }
+            if (options.hasOwnProperty('class')) {
+                element.setAttribute('class', options.class);
+            }
+            if (options.hasOwnProperty('text')) {
+                var textNode = document.createTextNode(options.text);
+                element.appendChild(textNode);
+            }
+            if (options.hasOwnProperty('parent')) {
+                addElementIntoParent(options, element);
+            } else {
+                document.body.appendChild(element);
+            }
+        } else {
+            console.log('Create element tag at lest');
+        }
+    }
+
+    function addElementIntoParent(options, element) {
+        if (parentCharacterCheck(options.parent)) {
+            console.log(true + ' : ' + options.parent);
+            parentElem = document.getElementsByTagName(options.parent)[0];
+            (parentElem !== undefined) ? parentElem.appendChild(element) : console.log((options.id === undefined ? options.class : options.id) + '\'s Parent is not Exist!');
+        } else {
+            var parentElem,
+                IdOrClassText = options.parent.substring(1, options.parent.length),
+                IdOrClassCheck = options.parent[0];
+            if (IdOrClassCheck === '#') {
+                console.log("ID");
+                parentElem = document.getElementById(IdOrClassText);
+                (parentElem !== null) ? parentElem.appendChild(element) : console.log((options.id === undefined ? options.class : options.id) + '\'s Parent is not Exist!');
+            } else if (IdOrClassCheck === '.') {
+                console.log("Class");
+                parentElem = document.getElementsByClassName(IdOrClassText)[0];
+                (parentElem !== undefined) ? parentElem.appendChild(element) : console.log((options.id === undefined ? options.class : options.id) + '\'s Parent is not Exist!');
+            }
+        }
+    }
+
+    function parentCharacterCheck(string) {
+        var re = new RegExp('^[a-zA-Z0-9]');
+        return re.test(string)
+    }
+}());
